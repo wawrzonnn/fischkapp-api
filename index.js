397d4856a0259a6818653d08ce62193ee352e8e3
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-08-07 22:07:17 +0200

    chore: connected with mongodb

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = process.env.PORT || 4000;
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/fischkapp';
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection failed:'));
+db.once('open', () => {
+  console.log('connection successful');
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
